fd2ff85c58fabd4e98a098a127956a2b4cab26b8
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-07-28 13:25:24 -0400

    Creé la sección de los proyectos💯

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="astro/client" />
+
+type Project = {
+    title: string,
+    body: string,
+    image: string,
+    repository: string,
+    preview?: string,
+    skills: SkillData[]
+}
+
+type SkillData = {
+    name: string,
+    path: string
+}
