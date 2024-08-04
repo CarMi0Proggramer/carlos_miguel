d16821ec811a7af23981196fd71f3b2c4eb8176b
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-08-04 05:29:14 -0400

    Hice la sección de contacto

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -1,18 +1,17 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-
-dotenv.config();
+import nodemailer from 'nodemailer';
+import { config } from "dotenv";
+config()
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.resend.com",
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-        user: "resend",
-        pass: process.env.RESEND_API_KEY
+        user: process.env.MY_EMAIL,
+        pass: process.env.EMAIL_PASS
     }
-});
+})
 
 transporter.verify()
-    .then(() => console.log("Ready to send emails"))
-    .catch(err => console.log(err))
+.then(() => console.log("Ready to send emails"))
+.catch(() => console.log("You can't send emails"))
--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,19 +1,28 @@
+import type { APIRoute } from "astro";
 import { transporter } from "../../config/mailer";
 
-export async function sendEmail(data: SendEmailData) {
-    let mailOptions = {
+export const POST: APIRoute = async ({ request }) => {
+    try {
+        const data = await request.formData();
+        await sendEmail({
+            name: data.get("name") as string,
+            email: data.get("email") as string,
+            message: data.get("message") as string,
+        })
+        
+        return new Response(JSON.stringify({ success: true }), { status: 200 });
+    } catch (err) {
+        return new Response(JSON.stringify({ success: false, err }), {
+            status: 500,
+        });
+    }
+} 
+
+async function sendEmail(data: SendEmailData) {
+    await transporter.sendMail({
         from: data.email,
         to: process.env.MY_EMAIL,
         subject: `Mensaje de ${data.name}`,
         text: data.message,
-    };
-
-    try {
-        await transporter.sendMail(mailOptions);
-        return new Response(JSON.stringify({ success: true }), { status: 200 });
-    } catch (error) {
-        return new Response(JSON.stringify({ success: false, error }), {
-            status: 500,
-        });
-    }
+    });
 }
