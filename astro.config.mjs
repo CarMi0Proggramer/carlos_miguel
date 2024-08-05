c82260bf2df6cdc5acc2efbeea9a33f4dd8cd0df
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-08-05 05:18:50 -0400

    cambié a vercel como adaptador

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
-import node from "@astrojs/node";
-
-//server
 export default defineConfig({
     integrations: [tailwind()],
     output: "server",
-    adapter: node({
-        mode: "standalone",
-    }),
+    adapter: vercel(),
 });
